880e273b95534f544dba84c6f330b6158720d583
牛梦林 <menglin.nml@ncarzone.com>
2023-11-21 00:01:27 +0800

    feat: QQ快捷一键登录实现

--- app/src/main/java/com/niu/jetpack_android_online/http/IApiInterface.kt
+++ app/src/main/java/com/niu/jetpack_android_online/http/IApiInterface.kt
@@ -2,12 +2,20 @@ package com.niu.jetpack_android_online.http
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import com.niu.jetpack_android_online.model.Author
 import com.niu.jetpack_android_online.model.Feed
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface IApiInterface {
 
+    /**
+     * 查看帖子列表
+     * @param feedId 帖子id，分页时传列表最后一个帖子的id
+     * @param feedType 帖子类型， all：全部类型； pics：仅图片类型；video：仅视频类型；text：仅文本类型
+     * @param pageCount 分页数量
+     * @param userId 当前登录者的id
+     */
     @GET("feeds/queryHotFeedsList")
     suspend fun getFeeds(
         @Query("feedId") feedId: Long = 0,
@@ -15,5 +23,20 @@ interface IApiInterface {
         @Query("pageCount") pageCount: Int = 10,
         @Query("userId") userId: Int = 0
     ): ApiResult<List<Feed>>
+
+    /**
+     * 创建或更新一个新的用户
+     * @param name 用户名
+     * @param avatar 用户头像
+     * @param qqOpenId qq登录后获得，代表用户的唯一身份
+     * @param expires_time 登录过期时间
+     */
+    @GET("user/insert")
+    suspend fun saveUser(
+        @Query("name") name: String,
+        @Query("avatar") avatar: String,
+        @Query("qq0openId") qqOpenId: String,
+        @Query("expires_time") expires_time: Long
+    ): ApiResult<Author>
 
 }
---
+++ app/src/main/java/com/niu/jetpack_android_online/pages/login/LoginActivity.kt
@@ -0,0 +1,102 @@
+package com.niu.jetpack_android_online.pages.login
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.niu.jetpack_android_online.base.BaseActivity
+import com.niu.jetpack_android_online.databinding.ActivityLayoutLoginBinding
+import com.niu.jetpack_android_online.http.ApiService
+import com.niu.jetpack_android_online.utils.showToast
+import com.tencent.connect.UserInfo
+import com.tencent.connect.common.Constants
+import com.tencent.tauth.IUiListener
+import com.tencent.tauth.Tencent
+import com.tencent.tauth.UiError
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+
+class LoginActivity : BaseActivity<ActivityLayoutLoginBinding>() {
+
+    private lateinit var tencent: Tencent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.actionClose.setOnClickListener { finish() }
+        binding.actionLogin.setOnClickListener { login() }
+
+        tencent = Tencent.createInstance("102075085", applicationContext)
+    }
+
+    private fun login() {
+        tencent.login(this, "all", loginListener)
+    }
+
+    private val loginListener = object : LoginListener() {
+        override fun onComplete(ret: Any) {
+            val response = ret as JSONObject
+            val openId = response.optString("openid")
+            val accessToken = response.optString("access_token")
+            val expiresIn = response.optString("expires_in")
+            tencent.openId = openId
+            tencent.setAccessToken(accessToken, expiresIn.toString())
+            getUserInfo()
+        }
+    }
+
+    private fun getUserInfo() {
+        val userInfo = UserInfo(applicationContext, tencent.qqToken)
+        userInfo.getUserInfo(object : LoginListener() {
+            override fun onComplete(any: Any) {
+                super.onComplete(any)
+                val response = any as JSONObject
+                val nickname = response.optString("nickname")
+                val avatar =
+                    response.optString("figureurl_2")//figureurl_1:50x50 figureurl_2:100x100
+                //将用户信息保存至服务器
+                save(nickname, avatar)
+            }
+        })
+    }
+
+    private fun save(nickname: String, avatar: String) {
+        lifecycleScope.launch {
+            val apiResult = ApiService.getService()
+                .saveUser(nickname, avatar, tencent.openId, tencent.expiresIn)
+            if (apiResult.success && apiResult.body != null) {
+                //关闭当前登录页面
+                finish()
+            } else {
+                //切换到主线程
+                withContext(Dispatchers.Main) {
+                    "登录失败".showToast()
+                }
+            }
+        }
+    }
+
+    private open inner class LoginListener : IUiListener {
+        override fun onComplete(p0: Any) {
+        }
+
+        override fun onError(err: UiError) {
+            "登录失败:reason${err.errorMessage}".showToast()
+        }
+
+        override fun onCancel() {
+            "登录失败".showToast()
+        }
+
+        override fun onWarning(p0: Int) {
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Constants.REQUEST_LOGIN) {
+            Tencent.onActivityResultData(requestCode, resultCode, data, loginListener)
+        }
+    }
+}
