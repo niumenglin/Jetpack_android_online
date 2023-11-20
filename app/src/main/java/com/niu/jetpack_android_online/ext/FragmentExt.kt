800064c8719368e289beab613db01d4873d85344
牛梦林 <menglin.nml@ncarzone.com>
2023-11-20 22:58:15 +0800

    feat: invokeViewBinding与invokeViewModel属性委托扩展实现

--- app/src/main/java/com/niu/jetpack_android_online/ext/FragmentExt.kt
+++ app/src/main/java/com/niu/jetpack_android_online/ext/FragmentExt.kt
@@ -1,11 +1,14 @@
 package com.niu.jetpack_android_online.ext
 
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
@@ -15,18 +18,33 @@ inline fun <reified VB : ViewBinding> invokeViewBinding() =
 
 @Suppress("UNCHECKED_CAST")
 class FragmentInflateBindingProperty<VB : ViewBinding>(private val clazz: Class<VB>) :
-    ReadOnlyProperty<Fragment, VB> {
+    ReadOnlyProperty<Any, VB> {
     private var binding: VB? = null
-    override fun getValue(thisRef: Fragment, property: KProperty<*>): VB {
+    override fun getValue(thisRef: Any, property: KProperty<*>): VB {
+        val layoutInflater: LayoutInflater?
+        val viewLifecycleOwner:LifecycleOwner?
+        when (thisRef){
+            is AppCompatActivity -> {
+                layoutInflater = thisRef.layoutInflater
+                viewLifecycleOwner = thisRef
+            }
+            is Fragment -> {
+                layoutInflater = thisRef.layoutInflater
+                viewLifecycleOwner = thisRef.viewLifecycleOwner
+            }
+            else -> {
+                throw java.lang.IllegalStateException("invokeViewBinding can only be used in AppCompatActivity or Fragment")
+            }
+        }
         if (binding == null) {
             try {
                 binding = (clazz.getMethod("inflate", LayoutInflater::class.java)
-                    .invoke(null, thisRef.layoutInflater) as VB)
+                    .invoke(null, layoutInflater) as VB)
             } catch (e: IllegalStateException) {
                 e.printStackTrace()
                 throw e
             }
-            thisRef.viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
                 override fun onDestroy(owner: LifecycleOwner) {
                     super.onDestroy(owner)
                     binding = null
@@ -43,9 +61,12 @@ inline fun <reified VM : ViewModel> invokeViewModel() =
     FragmentViewModelProperty(VM::class.java)
 
 class FragmentViewModelProperty<VM : ViewModel>(private val clazz: Class<VM>) :
-    ReadOnlyProperty<Fragment, VM> {
+    ReadOnlyProperty<Any, VM> {
     private var vm: VM? = null
-    override fun getValue(thisRef: Fragment, property: KProperty<*>): VM {
+    override fun getValue(thisRef: Any, property: KProperty<*>): VM {
+      if (thisRef !is ViewModelStoreOwner){
+          throw java.lang.IllegalStateException("invokeViewModel can only be used in ViewModelStoreOwner instance")
+      }
       if (vm == null){
           vm = ViewModelProvider(thisRef,ViewModelProvider.NewInstanceFactory())[clazz]
       }
